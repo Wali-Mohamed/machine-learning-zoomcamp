c14bfc965fd38d277d13f4de78bd75ffb8cce95d
Wali Mohamed <Wali-Mohamed@users.noreply.github.com>
2024-10-31 07:58:08 +0000

    finished homework

---
+++ 05-Deployment/homework-5/churn-pred-app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy
+
+customer={"job": "management", "duration": 400, "poutcome": "success"}
+print(customer)
+with open('dv.bin', 'rb') as f_in: # very important to use 'rb' here, it means read-binary 
+    dv = pickle.load(f_in)
+with open('model1.bin', 'rb') as f_in: # very important to use 'rb' here, it means read-binary 
+    model = pickle.load(f_in)
+## Note: never open a binary file you do not trust the source!
+print(dv)
+print(model)
+app = Flask('ping') # give an identity to your web service
+
+@app.route('/pred', methods=['POST']) # use decorator to add Flask's functionality to our function
+def predict():
+    request.get_json()
+    
+
+    X = dv.transform(customer)
+    y_pred = model.predict_proba(X)[:, 1]
+    churn=y_pred>=0.5
+    result={
+        'churn_probability':float(y_pred),
+        'churn':bool(churn)
+
+    }
+    return jsonify(result)
+    
+
+if __name__ == '__main__':
+   app.run(debug=True, host='0.0.0.0', port=9696) # run the code in local machine with the debugging mode true and port 9696
