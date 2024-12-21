11bc0c035728ada7420decd4f3d8e9ee771afe72
Wali Mohamed <Wali-Mohamed@users.noreply.github.com>
2024-12-21 00:11:29 +0000

    finished homework 10

--- 09-serverless/homework/test.py
+++ 09-serverless/homework/test.py
@@ -3,7 +3,8 @@ import requests
 url = 'http://localhost:8899/2015-03-31/functions/function/invocations'
 #url = 'http://localhost:8899'
 
-data = {'url': 'https://habrastorage.org/webt/yf/_d/ok/yf_dokzqy3vcritme8ggnzqlvwa.jpeg'}
+#data = {'url': 'https://habrastorage.org/webt/yf/_d/ok/yf_dokzqy3vcritme8ggnzqlvwa.jpeg'}
+data = {'url': 'https://pixabay.com/illustrations/girl-woman-portrait-face-fashion-8726241/'}
 
 result = requests.post(url, json=data).json()
 print(result)
